fc3ee530fb928a2ff6945cf31a4841b522536712
WeidongFang1985 <fangweidong1985@gmail.com>
2024-02-17 13:24:45 +1100

    Add LanguageButton Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
+// App.js
 import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import IconButton from '@mui/material/IconButton';
-import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import LanguageButton from "./LanguageButton";
 
 const App = () => {
   const [word, setWord] = useState('');
 
-  const speak = (lang) => {
+  const speak = (lang, word) => {
     const speech = new SpeechSynthesisUtterance(word);
     speech.lang = lang;
     window.speechSynthesis.speak(speech);
@@ -38,26 +38,9 @@ const App = () => {
         />
 
         <Box display="flex" alignItems="center" paddingLeft="10px" gap={2}>
-          <Box display="flex" alignItems="center" gap={0.25}>
-            <Typography color="primary">GB</Typography>
-            <IconButton color="primary" onClick={() => speak('en-GB')}>
-              <VolumeUpIcon />
-            </IconButton>
-          </Box>
-
-          <Box display="flex" alignItems="center" gap={0.25}>
-            <Typography color="secondary">US</Typography>
-            <IconButton color="secondary" onClick={() => speak('en-US')}>
-              <VolumeUpIcon />
-            </IconButton>
-          </Box>
-
-          <Box display="flex" alignItems="center" gap={0.25}>
-            <Typography sx={{ color: 'red' }}>CN</Typography>
-            <IconButton sx={{ color: 'red' }} onClick={() => speak('zh-CN')}>
-              <VolumeUpIcon />
-            </IconButton>
-          </Box>
+          <LanguageButton lang="en-GB" label="GB" color="primary" word={word} onClick={speak} />
+          <LanguageButton lang="en-US" label="US" color="secondary" word={word} onClick={speak} />
+          <LanguageButton lang="zh-CN" label="CN" color="error" word={word} onClick={speak} />
         </Box>
       </Container>
     </div>
---
+++ src/LanguageButton.js
@@ -0,0 +1,17 @@
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import Box from "@mui/material/Box";
+
+const LanguageButton = ({ lang, label, color, word, onClick }) => {
+	return (
+		<Box display="flex" alignItems="center" gap={0.25}>
+			<Typography color={color}>{label}</Typography>
+			<IconButton color={color} onClick={() => onClick(lang, word)}>
+				<VolumeUpIcon />
+			</IconButton>
+		</Box>
+	);
+};
+
+export default LanguageButton;
