80717a63ca1239c9b754f916a1af09c31c9f1b9b
WeidongFang1985 <fangweidong1985@gmail.com>
2024-02-17 12:45:40 +1100

    material UI

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { useState } from 'react';
-import "./App.css";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 
 const App = () => {
   const [word, setWord] = useState('');
 
   const speak = (lang) => {
     const speech = new SpeechSynthesisUtterance(word);
-    speech.lang = lang; // "en-US" for American English, "en-GB" for British English
+    speech.lang = lang;
     window.speechSynthesis.speak(speech);
   };
 
   return (
-    <div className="App">
-      <input type="text" value={word} onChange={(e) => setWord(e.target.value)} placeholder="输入单词" />
-      <button onClick={() => speak('en-GB')}>播放英式发音</button>
-      <button onClick={() => speak('en-US')}>播放美式发音</button>
-      <button onClick={() => speak('en-AU')}>播放澳式发音</button>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Word Pronunciation
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Container maxWidth="sm" style={{ marginTop: '20px' }}>
+        <TextField
+          fullWidth
+          label="输入单词"
+          variant="outlined"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+          margin="normal"
+        />
+
+        <Button variant="contained" color="primary" onClick={() => speak('en-GB')} style={{ marginRight: '10px' }}>
+          播放英式发音
+        </Button>
+        <Button variant="contained" color="secondary" onClick={() => speak('en-US')}>
+          播放美式发音
+        </Button>
+      </Container>
     </div>
   );
 };
